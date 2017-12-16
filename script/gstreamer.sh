56897ddcfcec4d0a90787dabc33bd0d2fc6f311b
Karthik Kumar <karthikkumar92@gmail.com>
2017-12-16 15:49:02 +0530

    Update gstreamer.sh

--- script/gstreamer.sh
+++ script/gstreamer.sh
@@ -2,4 +2,5 @@
 
 echo '***Installing Gsteamer***'
 dnf install -y gstreamer1-plugins-base gstreamer1-plugins-good gstreamer1-plugins-ugly gstreamer1-plugins-bad-free gstreamer1-plugins-bad-freeworld gstreamer1-plugins-bad-free-extras ffmpeg
+dnf --enablerepo=updates-testing update gstreamer1-plugins-ugly
 echo
